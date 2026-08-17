7054e1bbe45d594247efe61483b37394e8581d08
henrique <henriqueferreirasantos2022@gmail.com>
2026-08-17 20:28:30 -0300

    Add files via upload

---
+++ 2.5.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+int main() { 
+    int dia = 30;
+    
+    switch (dia){
+        case 1:
+            printf("Janeiro");
+            break;
+        case 2:
+            printf("fevereiro");
+            break;
+        case 3:
+            printf("março");
+            break;
+        case 4:
+            printf("abril");
+            break;
+        case 5:
+            printf("maio");
+            break;
+        case 6:
+            printf("junho");
+            break;
+        case 7:
+            printf("julho");
+            break;
+        case 8:
+            printf("agosto");
+            break;
+        case 9:
+            printf("setembro");
+            break;
+        case 10:
+            printf("outubro"); 
+            break;
+        case 11:
+            printf("novembro");
+            break;
+        case 12:
+            printf("dezembro");
+            break;
+        default:
+        printf("Mês inválido! Digite apenas números de 1 a 12.");
+            break;
+    }
+    
+    return 0; 
+    
+}
