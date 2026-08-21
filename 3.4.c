3aa670dd6bb223e018370d9f26c3613bd11205d8
henrique <henriqueferreirasantos2022@gmail.com>
2026-08-20 23:40:03 -0300

    Add files via upload

---
+++ 3.4.c
@@ -0,0 +1,27 @@
+
+#include <stdio.h>
+
+//Leia um número inteiro N e conte quantos números pares existem entre 1 e N.
+
+int main()
+{
+    int quantidade, contador = 0, par;
+    
+    printf("Até que número vc quer saber a quantidade de par? ");
+    scanf("%d", &quantidade);
+    
+    for (int contador = 1; contador <= quantidade; contador++) {
+        if (contador % 2 == 0) {
+            printf("%d\n", contador);
+        }
+}
+    
+    
+    
+    
+    
+    
+    
+    
+
+}
